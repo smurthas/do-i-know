70af97c57548be768ea280c862dc2a0f81438738
Jeremie Miller <jeremie@jabber.org>
2012-05-17 16:16:40 -0500

    adding fb, instagram, 4sq, async, and enabling

--- js/api.js
+++ js/api.js
@@ -2,7 +2,38 @@ if (typeof DoIKnow === 'undefined') DoIKnow = {};
 
 api = {
   facebook: function(done) {
-    
+    $.getJSON(DoIKnow.Util.api('/services/facebook/friends'), {
+      access_token: DoIKnow.Util.accessToken(),
+      limit:1000
+    }, function(list) {
+      _.each(list, function(entry){
+        DB.saveName(entry.data.name);
+      });
+      done();
+    });
+  },
+  instagram: function(done) {
+    $.getJSON(DoIKnow.Util.api('/services/instagram/follows'), {
+      access_token: DoIKnow.Util.accessToken(),
+      limit:1000
+    }, function(list) {
+      _.each(list, function(entry){
+        DB.saveName(entry.data.full_name);
+      });
+      done();
+    });
+  },
+  foursquare: function(done) {
+    $.getJSON(DoIKnow.Util.api('/services/foursquare/friends'), {
+      access_token: DoIKnow.Util.accessToken(),
+      limit:1000
+    }, function(list) {
+      _.each(list, function(entry){
+        DB.saveName(entry.data.firstName+' '+entry.data.lastName);
+        if(entry.data.contact && entry.data.contact.email) DB.saveEmail(entry.data.contact.email);
+      });
+      done();
+    });
   }
 }
 
@@ -11,8 +42,15 @@ $(function() {
   $.getJSON(DoIKnow.Util.api('/profiles'), {
     access_token: DoIKnow.Util.accessToken()
   }, function(profiles) {
-    _.each(Object.keys(profiles), function(service) {
-      if(api[service]) api[service](function(){console.log("DONE with "+service)});
+    console.log("PROFILES",profiles);
+    async.forEach(Object.keys(profiles), function(service, cb) {
+      if(api[service]) api[service](function(){
+        console.log("DONE with "+service);
+        cb();
+      });
+    }, function(){
+      console.log("ALL DONE NOW");
+      DoIKnow.loaded = true;
     });
   });
 });
